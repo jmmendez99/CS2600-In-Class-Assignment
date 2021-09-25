ea6003668f779b125a9e3d7f7a61931e6f4ce869
Justin Mendez <jmmendez@cpp.edu>
2021-09-24 23:34:16 -0700

    Initial Number_Guessing.c File and plan

---
+++ Number_Guessing.c
@@ -0,0 +1,16 @@
+/*
+--The whole program can probably be in multiple while loops that keeps the player
+  in the game until they press '3'
+--For option 1 create a while loop that keeps the player in the guessing stage
+  until they guess the right number or press 'q'. Use if statements to check if the user
+  number is the right number. If they get it right use a continue/break statement that will
+  push them out of the option 1 stage loop and to the outer loop which will prompt
+  them again with the 3 original options.
+--For option 2 display what the max value the user can set a the number to. Can create
+  a while loop that keep them in option 2 stage until they input a number that is 
+  less than or equal to the max number and not a negative number/less than 0. Once they
+  do put in a right number, continue/break the loop to move the user out of option 2 stage.
+--For option 3 print Thank You for Playing and exit the program.
+--Try to save max number of the program to a file and then read that number back into the program
+  when you start it up again.
+*/
